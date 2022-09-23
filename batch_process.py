9fc6af5913ad8efca2a2f410f0d0b0831a9015d0
Raihan Nurrasyid <98638367+MasRe99@users.noreply.github.com>
2022-09-23 13:27:51 +0700

    Update batch_process.py

--- batch_process.py
+++ batch_process.py
@@ -1,55 +1,27 @@
 import glob
 from PIL import Image
 
+#To set the coordinates as the boundaries
 box1 = (300, 300, 1050, 1050)
 box2 = (1050, 300, 1800, 1050)
 box3 = (300, 1050, 1050, 1800)
 box4 = (1050, 1050, 1800, 1800)
 
+#function to crop the image
 def compress_image(source_path, dest_path):
     with Image.open(source_path) as img:
         image_cropped1 = img.crop(box1)
         image_cropped2 = img.crop(box2)
         image_cropped3 = img.crop(box3)
         image_cropped4 = img.crop(box4)
+#auto save with
+        image_cropped1.save(dest_path[:-4] + "-C1" + ".JPG", "JPEG")
+        image_cropped2.save(dest_path[:-4] + "-C2" + ".JPG", "JPEG")
+        image_cropped3.save(dest_path[:-4] + "-C3" + ".JPG", "JPEG")
+        image_cropped4.save(dest_path[:-4] + "-C4" + ".JPG", "JPEG")
 
-        image_cropped1.save(dest_path[:-4] + "1" + ".JPG", "JPEG")
-        image_cropped2.save(dest_path[:-4] + "2" + ".JPG", "JPEG")
-        image_cropped3.save(dest_path[:-4] + "3" + ".JPG", "JPEG")
-        image_cropped4.save(dest_path[:-4] + "4" + ".JPG", "JPEG")
-
-
+#To batch processing
 paths = glob.glob("*.JPG")
 
 for path in paths:
     compress_image(path, path[:-4]+ ".JPG")
-
-"""import glob
-from PIL import Image
-
-box1 = (300, 300, 1050, 1050)
-box2 = (1050, 300, 1800, 1050)
-box3 = (300, 1050, 1050, 1800)
-box4 = (1050, 1050, 1800, 1800)
-
-def crop_image(source_path, dest_path):
-    with Image.open(source_path) as img:
-        img_crop1 = img.crop(box1)
-        img_crop.save(dest_path)
-
-paths = glob.glob(".JPG")
-for path in paths:
-    crop_image(path, path[:-4] + ".JPG")
-"""
-"""for image in glob.glob("Dataset/*.jpg"):
-    print("Processing {}...".format(image))
-    im1_crop = image.crop(box1)
-    im2_crop = image.crop(box2)
-    im3_crop = image.crop(box3)
-    im4_crop = image.crop(box4)
-
-    im_save = im1_crop.save("cropped1_" + image)
-    im_save = im2_crop.save("cropped2_" + image)
-    im_save = im3_crop.save("cropped3_" + image)
-    im_save = im4_crop.save("cropped4_" + image)
-"""
